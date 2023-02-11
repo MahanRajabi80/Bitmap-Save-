693488c531ca86ae0531cd00d64ed82c9d1bde60
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 20:05:59 +0330

    Add files via upload

---
+++ Example 17-17/Example 17-17/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Example_17_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblPicAll = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(392, 468);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 42);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(311, 468);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 42);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblPicAll
+            // 
+            this.lblPicAll.AllowDrop = true;
+            this.lblPicAll.BackColor = System.Drawing.Color.White;
+            this.lblPicAll.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPicAll.Location = new System.Drawing.Point(120, 19);
+            this.lblPicAll.Name = "lblPicAll";
+            this.lblPicAll.Size = new System.Drawing.Size(515, 446);
+            this.lblPicAll.TabIndex = 2;
+            this.lblPicAll.DragDrop += new System.Windows.Forms.DragEventHandler(this.lblPicAll_DragDrop);
+            this.lblPicAll.DragEnter += new System.Windows.Forms.DragEventHandler(this.lblPicAll_DragEnter);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox7);
+            this.groupBox1.Controls.Add(this.pictureBox6);
+            this.groupBox1.Controls.Add(this.pictureBox5);
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(108, 520);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pictures";
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::Example_17_17.Properties.Resources.Penguins;
+            this.pictureBox7.Location = new System.Drawing.Point(6, 445);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(94, 65);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxSmall_MouseDown);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Example_17_17.Properties.Resources.Lighthouse;
+            this.pictureBox6.Location = new System.Drawing.Point(6, 374);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(94, 65);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxSmall_MouseDown);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Example_17_17.Properties.Resources.Koala;
+            this.pictureBox5.Location = new System.Drawing.Point(7, 303);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(94, 65);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxSmall_MouseDown);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Example_17_17.Properties.Resources.Jellyfish;
+            this.pictureBox4.Location = new System.Drawing.Point(6, 232);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(94, 65);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxSmall_MouseDown);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Example_17_17.Properties.Resources.Hydrangeas;
+            this.pictureBox3.Location = new System.Drawing.Point(7, 161);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(94, 65);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxSmall_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Example_17_17.Properties.Resources.Desert;
+            this.pictureBox2.Location = new System.Drawing.Point(6, 90);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(94, 65);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxSmall_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Example_17_17.Properties.Resources.Chrysanthemum;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxSmall_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 520);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblPicAll);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Example 17-17 (Bitmap Save)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label lblPicAll;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+    }
+}
+
---
+++ Example 17-17/Example 17-17/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_17_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //------------------------------------------------------------------------
+        Bitmap NullPicture;
+        //------------------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;
+            Graphics gr=lblPicAll.CreateGraphics();
+            NullPicture = new Bitmap(lblPicAll.Width, lblPicAll.Height, gr);
+            lblPicAll.Image = NullPicture;
+        }
+        //------------------------------------------------------------------------
+        private void pictureBoxSmall_MouseDown(object sender, MouseEventArgs e)
+        {
+            //har kodam az tasavir kochak
+            PictureBox pic = (PictureBox)sender;
+            DoDragDrop(pic.Image, DragDropEffects.Copy);
+        }
+        //------------------------------------------------------------------------
+        private void lblPicAll_DragDrop(object sender, DragEventArgs e)
+        {
+            //tasvir asli
+            int x, y;
+            x = e.X - this.Left - lblPicAll.Left;
+            y = e.Y - this.Top - lblPicAll.Top - 32;
+            Bitmap pic_in = (Bitmap)e.Data.GetData(typeof(Bitmap));
+            Bitmap bmplabel = new Bitmap(lblPicAll.Image, lblPicAll.Width, lblPicAll.Height);
+            Graphics gr = Graphics.FromImage(bmplabel);
+            //vaghti b gerefte mishe ro graphice tasvir kar mishe , bad mishe tasviro zakhire kard
+            gr.DrawImage(pic_in, x, y, pictureBox1.Width, pictureBox1.Height);
+            lblPicAll.Image = bmplabel;
+        }
+        //------------------------------------------------------------------------
+        private void lblPicAll_DragEnter(object sender, DragEventArgs e)
+        {
+            //tasvir asli
+            if (e.Data.GetDataPresent(typeof(Bitmap)))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        //------------------------------------------------------------------------
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear
+            lblPicAll.Image = NullPicture;
+        }
+        //------------------------------------------------------------------------
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //save
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Jpeg Files (*.jpg)|*.jpg|Bitmap Files (*.bmp)|*.bmp";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "" && lblPicAll.Image!=null)
+            {
+                lblPicAll.Image.Save(saveFileDialog1.FileName);
+                MessageBox.Show("Data is Saved!", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }        
+    }
+}
